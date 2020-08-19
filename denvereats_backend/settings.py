669fd57703898af1f11ccb7137c25960d4c1e6ba
kathryn0908 <kathrynlael@gmail.com>
2020-08-19 14:30:15 -0600

    transfer from sqlite to postgres

--- denvereats_backend/settings.py
+++ denvereats_backend/settings.py
@@ -84,9 +84,17 @@ WSGI_APPLICATION = 'denvereats_backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'capstone_backend',
+        'USER': 'katie_admin',
+        'PASSWORD': 'K_1992',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
